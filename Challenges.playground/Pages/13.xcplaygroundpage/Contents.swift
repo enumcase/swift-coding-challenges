2c7a8ded5be51d127ac85b4b3882e15a15523347
enumcase <markdaribayev@gmail.com>
2021-05-05 15:26:07 +0600

    Added Challenge 13

--- Challenges.playground/Pages/13.xcplaygroundpage/Contents.swift
+++ Challenges.playground/Pages/13.xcplaygroundpage/Contents.swift
@@ -1,7 +1,44 @@
-//: [Previous](@previous)
+/*
+ 
+ Challenge 13: Run-length encoding
 
+ Write a function that accepts a string as input, then returns how often each letter is repeated in a single run, taking case into account.
+ Tip: This approach is used in a simple lossless compression technique called run-length encoding.
+ 
+ Sample input and output
+ • The string “aabbcc” should return “a2b2c2”.
+ • The strings “aaabaaabaaa” should return “a3b1a3b1a3”
+ • The string “aaAAaa” should return “a2A2a2”
+
+ */
 import Foundation
 
-var str = "Hello, playground"
+func challenge13(_ str: String) -> String {
+    var result = ""
+    var currentChar: Character? = nil
+    
+    var count = 0
+    
+    for char in str {
+        if char == currentChar {
+            count += 1
+        } else {
+            if let currentChar = currentChar {
+                result += ("\(currentChar)\(count)")
+            }
+            
+            currentChar = char
+            count = 1
+        }
+    }
+    
+    if let currentChar = currentChar {
+        result += ("\(currentChar)\(count)")
+    }
+    
+    return result
+}
 
-//: [Next](@next)
+assert(challenge13("aabbcc") == "a2b2c2", "Challenge 13 failed")
+assert(challenge13("aaabaaabaaa") == "a3b1a3b1a3", "Challenge 13 failed")
+assert(challenge13("aaAAaa") == "a2A2a2", "Challenge 13 failed")
